f1a74cc14dc06f9c12baef6f93f938c4c766780c
newPacAttack <91351390+newPacAttack@users.noreply.github.com>
2021-12-19 10:01:37 -0700

    Add files via upload

---
+++ Plantilla-Proyecto-C36-main/Proyecto 26/sketch.js
@@ -0,0 +1,70 @@
+var dog,sadDog,happyDog, database;
+var foodS,foodStock;
+var addFood;
+var foodObj;
+
+//crea aquí las variables feed y lastFed 
+  var feed, lastFed;
+
+function preload(){
+sadDog=loadImage("Dog.png");
+happyDog=loadImage("happy dog.png");
+}
+
+function setup() {
+  database=firebase.database();
+  createCanvas(1000,400);
+
+  foodObj = new Food();
+
+  foodStock=database.ref('Food');
+  foodStock.on("value",readStock);
+  
+  dog=createSprite(800,200,150,150);
+  dog.addImage(sadDog);
+  dog.scale=0.15;
+
+  //crea aquí el boton Alimentar al perro
+  feed=createButton("Alimenta al Perro")
+  feed.position(850,95);
+  feed.mousePressed(feed);
+  addFood=createButton("Agregar Alimento");
+  addFood.position(800,95);
+  addFood.mousePressed(addFood);
+
+}
+
+function draw() {
+  background(46,139,87);
+  foodObj.display();
+
+  //escribe el código para leer el valor de tiempo de alimentación de la base de datos
+  
+ 
+  //escribe el código para mostrar el texto lastFed time aquí
+
+ 
+  drawSprites();
+}
+
+//función para leer la Existencia de alimento
+function readStock(data){
+  foodS=data.val();
+  foodObj.updateFoodStock(foodS);
+}
+
+
+function feedDog(){
+  dog.addImage(happyDog);
+
+  //escribe el código aquí para actualizar las existencia de alimento, y la última vez que se alimentó al perro
+
+}
+
+//funcón para agregar alimento al almacén
+function addFoods(){
+  foodS++;
+  database.ref('/').update({
+    Food:foodS
+  })
+}
